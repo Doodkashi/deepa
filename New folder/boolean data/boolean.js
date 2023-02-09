5db5a5adbe4c9b92e591576269142c1c4a4ad37e
doodkashi <suryavamshigayathri@gmail.com>
2023-02-09 17:35:59 +0530

    boolean

---
+++ New folder/boolean data/boolean.js
@@ -0,0 +1,32 @@
+// a is greater than b
+let a=6
+let b=4
+
+console.log(a>b)
+
+// x is less than y false
+let x=3
+let y=1
+
+console.log(x<y)
+
+
+// m,n is 4
+let m=4
+let n=6
+
+console.log(m=n)
+
+// s and t are equal
+let s=7
+let t="7"
+
+console.log(s==t)
+
+// u is not equal to v
+let u=2
+let v=1
+
+console.log(u!=v)
+
+
