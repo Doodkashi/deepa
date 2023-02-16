b9b62a6c8d54bf6544694966de1d2bba4f036b92
doodkashi <suryavamshigayathri@gmail.com>
2023-02-16 18:45:25 +0530

    string

---
+++ New folder/datatypes/string.js
@@ -0,0 +1,33 @@
+//placed in double quotes
+let car="volvo"
+
+console.log(car)
+
+// plased in single quotes
+let name='gayathri'
+
+console.log(name)
+
+// given variables x,y,z
+let x='mango'
+let y='gauva'
+let z='apple'
+
+console.log(x,y,z)
+
+console.log(x)
+
+console.log(y)
+
+console.log(z)
+
+// here it defined length
+let text="abcdefghijklmnopqrstuvwxyz"
+
+console.log(text.length)
+
+// m and n are objects
+let m="amarnath"
+let n="amarnath"
+
+console.log(m===n)
