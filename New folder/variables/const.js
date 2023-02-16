f9dee5bb44da70c9d84d178b83305b1b1ef7d21c
doodkashi <suryavamshigayathri@gmail.com>
2023-02-16 21:49:44 +0530

    const

---
+++ New folder/variables/const.js
@@ -0,0 +1,45 @@
+// here names are using for loop in
+const names=["gayathri","deepa","laxmi","deepika"]
+
+console.log(names)
+
+// here fruits are using for loop in
+const fruits=["banana","orange","pineapple"]
+
+console.log(fruits)
+
+// here marks are using for loop in
+const marks=[23,24,25,22,21]
+
+console.log(marks)
+
+
+// here person are using for loop in
+const person1="amarnath"
+const person2="maharaju"
+const person3="charan"
+
+console.log(person1)
+
+console.log(person2)
+
+console.log(person3)
+
+console.log(person1+person2+person3)
+
+
+// m,n,o are variables
+const m=123
+const n=456
+const o=789
+
+console.log(m)
+
+console.log(n)
+
+console.log(o)
+
+console.log(m*n)
+
+console.log(m+o)
+
