0290e89297889e64a84608e69471898560bba941
doodkashi <suryavamshigayathri@gmail.com>
2023-02-16 22:07:46 +0530

    numbers

---
+++ New folder/datatypes/numbers.js
@@ -0,0 +1,45 @@
+// used decimal number
+let x=3.123
+let y=3
+
+console.log(x)
+
+console.log(y)
+
+console.log(x*y)
+
+//used decimal number
+let m=2.0
+let n=23.98
+
+console.log(m*n)
+
+console.log(m/n)
+
+
+// here fruit name is used
+let o=9.877
+let p="apple"
+
+console.log(o+p)
+
+console.log(o/p)
+
+// here NaN is used
+let k=NaN
+let s=100
+
+console.log(k+s)
+
+
+
+let h=765
+let t=675
+
+console.log(h)
+
+console.log(h*t)
+
+console.log(h/t)
+
+console.log(h+t)
