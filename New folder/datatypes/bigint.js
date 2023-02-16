ab7ccfd266cdb9993bd06d6a1b56970819cc1abb
doodkashi <suryavamshigayathri@gmail.com>
2023-02-16 22:30:49 +0530

    bigint

---
+++ New folder/datatypes/bigint.js
@@ -0,0 +1,40 @@
+ // here typeof bigint
+ let x= BigInt(6423789348940736365)
+ let type= typeof x;
+
+ console.log(typeof x)
+
+
+// here bigint is used
+ let m=12345678909876544333n;
+ let n=BigInt(12345678909876544333)
+
+ console.log(m)
+
+ console.log(m,n)
+
+// here o,p are variables
+ let o=522367565483253765724653245
+ let p=687365324903829478947386632
+
+ console.log(o/p)
+
+
+//here k,l are variables
+ let k=738264752634754865975984570
+ let l=876238735485085098937487783
+
+ console.log(k*l)
+
+// here decimals are used
+ let u=0.8921897928637825623
+ let v=78.764873619463986587
+
+ console.log(u+v)
+
+ console.log(u-v)
+
+ console.log(u==v)
+
+
+ 
