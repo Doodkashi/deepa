ce09d5dc39a22bf6b1e530ba7054f898ca3c642f
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 18:24:36 +0530

    null

---
+++ New folder/datatypes/null.js
@@ -0,0 +1,26 @@
+// here name is using for loop in
+let name="null"
+
+console.log(null)
+
+console.log(name)
+
+// here myvariable iis using for loop in
+let myvariable='null'
+
+console.log(myvariable)
+
+// here fruit is using for loop in
+let fruit='null'
+
+console.log(fruit)
+
+//here x is an variable
+let x='null'
+
+console.log(x)
+
+// here y is an variable
+let m='null'
+
+console.log(m)
