76770004c4cff27b6edbab76f181728a79e7a47d
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 12:14:02 +0530

    undefined

---
+++ New folder/datatypes/undefined.js
@@ -0,0 +1,32 @@
+// here value and type is also undefined
+let name;
+
+console.log(typeof name)
+
+// here value and type is also undefined
+let car="volvo";
+car=undefined;
+
+console.log(car)
+
+
+// here the type of value is string
+let x="";
+
+console.log(typeof x)
+
+// here book id undefined
+let book;
+
+console.log(book)
+
+
+// here a and b are undefined
+let a;
+let b;
+
+console.log(a,b)
+
+console.log(a)
+
+console.log(b)
