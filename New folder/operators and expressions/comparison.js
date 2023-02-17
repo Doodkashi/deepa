9142c3e36741508b8c4d53ab81956bfba8cf6713
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 19:32:09 +0530

    comparision operator

---
+++ New folder/operators and expressions/comparison.js
@@ -0,0 +1,57 @@
+// == is comparison operator is used
+let a=0
+let b=0
+
+console.log(a==b)
+
+// === is comparison operator is used
+let x="ramesh"
+let y="ramesh"
+
+console.log(x===y)
+
+// != is comparison operator is used
+let m=0
+let n=9
+
+console.log(m!=n)
+
+// !== is comparison operator is used
+let o="8"
+let p=45
+
+console.log(o!==p)
+
+console.log(o===p)
+
+// > is comparison operator is used
+let r=9
+let s=8
+
+console.log(r>s)
+
+// < is comparison operator is used
+let w=3
+let z=2
+
+console.log(w<z)
+
+// >= is comparison operator is used
+let k=54
+let l=98
+
+console.log(k>=l)
+
+// <= is comparison operator is used
+let j=78
+let h=76
+
+console.log(j<=h)
+
+
+let u;
+
+console.log(u)
+
+
+
