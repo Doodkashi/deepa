3b8b75df096aa63b94282f0f590d3c2b8f3f6996
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 18:51:21 +0530

    arithmetic operator

---
+++ New folder/operators and expressions/arithmetic.js
@@ -0,0 +1,44 @@
+// + arithmetic operator is used
+let x=1;
+let y=3;
+
+console.log(x+y)
+
+// - arithmetic operator is used
+let a=9;
+let b=7;
+
+console.log(a-b)
+
+// * arithmetic operator is used
+let m=4;
+let n=5;
+
+console.log(m*n)
+
+
+// ** arithmetic operator is used
+let k=3;
+let l=4;
+
+console.log(k**l)
+
+// / arithmetic operator is used
+let g=8;
+let h=5;
+
+console.log(g/h)
+
+// % arithmetic operator is used
+let u=3;
+let v=6;
+
+console.log(u%v)
+
+
+let o=5;
+let p=6;
+
+console.log(o++)
+
+console.log(p--)
