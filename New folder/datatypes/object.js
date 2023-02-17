3721316bc26832e50e31db9df7e3bfa7f7638e36
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 12:58:01 +0530

    object

---
+++ New folder/datatypes/object.js
@@ -0,0 +1,32 @@
+// person is defined as object
+const person={ name:"deepa", age:21, color:"red"}
+
+console.log(person.name + "is" + person.age)
+
+console.log(person.name + "is" + person.age + "years old")
+
+
+// fruit is defined as object
+const fruit={fruit:"banana", color:"yellow"}
+
+console.log(fruit.fruit + "is" + fruit.color + "in color")
+
+// here color is used
+let color={name:"pink"}
+
+console.log(color)
+
+console.log(color.name)
+
+
+// here m variable is used
+var m={h:"1"}
+
+console.log(m)
+
+console.log(m.h)
+
+
+
+
+
