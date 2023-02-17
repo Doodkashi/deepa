884135bd6baf7f9104a6d633e35e53113c456d72
doodkashi <suryavamshigayathri@gmail.com>
2023-02-17 19:05:40 +0530

    assignment operator

---
+++ New folder/operators and expressions/assignment.js
@@ -0,0 +1,45 @@
+// += assignment operator is used
+let a=34;
+let b=43;
+
+console.log(a+=b)
+
+// -= assignment operator is used
+let x=87;
+let y=78;
+
+console.log(x-=y)
+
+// = assignment operator is used
+let m=43;
+let n=87;
+
+console.log(m=n)
+
+
+// *= assignment operator is used
+let o=56;
+let p=43;
+
+console.log(o*=p)
+
+// /= assignment operator is used
+let g=65;
+let h=78;
+
+console.log(g/=h)
+
+// %= assignment operator is used
+let r=77;
+let s=98;
+
+console.log(r%=s)
+
+// ** assignment operator is used
+let c=88;
+let d=98;
+
+console.log(c**d)
+
+console.log(c**=d)
+
