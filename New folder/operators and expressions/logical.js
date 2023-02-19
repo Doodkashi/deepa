8fb1e06b8576967f4930f7f315dc31db1c7bfab1
doodkashi <suryavamshigayathri@gmail.com>
2023-02-19 18:18:06 +0530

    logical operator

---
+++ New folder/operators and expressions/logical.js
@@ -0,0 +1,27 @@
+// here && is logical operator
+let x=9
+let y=8
+
+console.log(x<4 && y>9)
+
+console.log(x>7 && y<9)
+
+// here || is logical operator
+let m=6
+let n=8
+
+console.log(m==1 || n==8)
+console.log(m==6 || n==0)
+
+console.log(m==7 || n==9)
+
+console.log(m==5 || n==6)
+
+
+
+let r=3
+let s=4
+
+console.log(r!==s)
+
+console.log(r==!s)
