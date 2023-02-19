42a2d7660e7c8cebd08dc4cacd3689826ceda2f8
doodkashi <suryavamshigayathri@gmail.com>
2023-02-19 18:59:30 +0530

    type operator

---
+++ New folder/operators and expressions/type.js
@@ -0,0 +1,34 @@
+// it is type operator
+let name="a"
+
+console.log(typeof name)
+
+// it is type operator
+let x=5
+let y=8
+
+console.log(typeof x)
+
+console.log(typeof y)
+
+
+// it is type operator
+let m=10
+
+console.log(typeof m)
+
+// it is type operator
+let v;
+
+console.log(typeof v)
+
+
+// it is type operator
+let u=56453434252556777687;
+
+console.log(typeof u)
+
+
+let a=!
+
+console.log(typeof !)
