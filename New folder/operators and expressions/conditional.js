35f2afed7cf39e45311ddd658c6044c3818f7731
doodkashi <suryavamshigayathri@gmail.com>
2023-02-19 18:43:12 +0530

    conditional

---
+++ New folder/operators and expressions/conditional.js
@@ -0,0 +1,20 @@
+// typeof condition
+let name="gayathri"
+
+console.log(typeof name)
+
+// typeof condition
+let x=5;
+
+console.log(typeof x)
+
+let v=6
+let u=7
+
+console.log(typeof v==u)
+
+console.log(typeof v)
+
+
+
+
