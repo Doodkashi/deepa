d8060f1759dd1ac22e22b1a005284187c64542e2
doodkashi <suryavamshigayathri@gmail.com>
2023-02-19 21:35:41 +0530

    for loop

---
+++ New folder/control structres/loop for.js
@@ -0,0 +1,24 @@
+// here for loop is used
+let boys=[1,2,3,4,5]
+for(let x of boys){
+    console.log(x)
+}
+
+// here for loop is used
+let fruits=["mango","banana","orange","apple","kiwi"]
+for(let i of fruits){
+    console.log(i)
+}
+
+
+// here for loop is used
+let family=["mother","father","daughter","bothers","grandmother","grandfather","sister"]
+for(let m of family){
+    console.log(m)
+}
+
+
+let brands=["dell","lenovo","samsung","logitech"]
+for(let n of brands){
+    console.log(n)
+}
