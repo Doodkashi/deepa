f021e027e5bd6a71edffffab7b0f559a0870f707
doodkashi <suryavamshigayathri@gmail.com>
2023-02-20 15:51:44 +0530

    for in loop

---
+++ New folder/control structres/loop for-in.js
@@ -0,0 +1,20 @@
+let numbers=[1,2,3,4,5,6]
+
+for(let num in numbers){
+    console.log(num)
+}
+
+
+let animals=["monkey","donkey","pig","giraf"]
+
+for(let names in animals){
+    console.log(names)
+}
+
+
+
+let a=[1234567890]
+
+for(let value in a){
+    console.log(value)
+}
