fac09066d81e8faa6c0af6cb3f572006ff010582
doodkashi <suryavamshigayathri@gmail.com>
2023-02-20 13:02:24 +0530

    for of loop

---
+++ New folder/control structres/loop for-of.js
@@ -0,0 +1,28 @@
+// here for of loop
+const array=[1,2,3,4]
+
+for(const element of array){
+    console.log(element)
+}
+
+// here for of loop
+const names=["radha","krishna","parvathi","laxmi"]
+
+for(const persons of names){
+    console.log(persons)
+}
+
+
+// here for of loop
+const fruits=["mango","banana","apple","grapes"]
+
+for(const fruit1 of fruits){
+    console.log(fruit1)
+}
+
+
+let subjects=["telugu","Hindi","English","maths","science","social"]
+
+for(let books of subjects){
+    console.log(books)
+}
