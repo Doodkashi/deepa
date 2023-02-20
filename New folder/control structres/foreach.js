5135251cbea0d99890023b910f7d6d97e4eebe2b
doodkashi <suryavamshigayathri@gmail.com>
2023-02-20 22:28:24 +0530

    foreach loop

---
+++ New folder/control structres/foreach.js
@@ -0,0 +1,24 @@
+// foreach loop
+let numbers = [1,2,3,4,5,6,7]
+
+numbers.forEach(function(number) {
+    console.log(number);
+});
+
+
+// foreach loop
+let names = ["mofan","raju","kiran","sweety"]
+
+names.forEach(function(names){
+     console.log(names);
+});
+
+
+//foreach loop
+let evennumbers = [4,6,8,10,12,14,16]
+
+evennumbers.forEach(function(evennumbers){
+       console.log(evennumbers);  
+});
+
+
