2f93333fa1acbde4b3b00cec21a7ef4a830f2ae2
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 16:35:55 +0530

    slice

---
+++ New folder/array methods/array slice.js
@@ -0,0 +1,13 @@
+let fruit = "mango"
+
+console.log(fruit.slice(2,4))
+
+
+let name = "gayathri"
+
+console.log(name.slice(1))
+
+
+let numbers ="12345678-------------------------"
+
+console.log(numbers.slice(3))
