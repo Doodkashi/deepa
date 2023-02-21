b557520e84791770e547e87a3d00274f5a9b898d
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 14:52:11 +0530

    array

---
+++ New folder/array/array.js
@@ -0,0 +1,11 @@
+const names = ["mounika","roopa","deepa","gayathri"]
+
+console.log(names)
+
+const animals = ["monkey","donkey","pig","lion"];
+
+console.log(animals)
+
+const books = ["ramayan","bible","quran"];
+
+console.log(books)
