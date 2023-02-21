ec18932be735b9ab48359618bf170f1f0f867aa2
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 21:27:26 +0530

    delete array

---
+++ New folder/array methods/array delete.js
@@ -0,0 +1,11 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
+
+console.log(fruits)
+
+
+
+const names = ["Banana", "Orange", "Apple", "Mango"];
+delete names[2];
+
+console.log(names)
