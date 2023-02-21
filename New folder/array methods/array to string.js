1349909f521e8f833ba0368e65c13cbd662a88c4
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 18:29:07 +0530

    array string

---
+++ New folder/array methods/array to string.js
@@ -0,0 +1,17 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.join(" * ");
+
+console.log(fruits.join(" * "))
+
+
+const names = ['a','b','c','d']
+names.toString('');
+
+console.log(names.toString(''));
+
+
+
+const members = [1,2,3,4,5,6,7,8]
+members.join(" * ")
+
+console.log(members.join(" * "))
