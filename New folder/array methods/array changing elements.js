23745680a569cf5017a50e6e40fe7081b638466e
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 21:19:26 +0530

    changing element

---
+++ New folder/array methods/array changing elements.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[0] = "Kiwi";
+
+console.log(fruits)
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+fruits2[3] = "Kiwi";
+
+console.log(fruits2)
