a139db698f3b3061ea9058403223f6c6784c86e9
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 16:22:09 +0530

    array length

---
+++ New folder/array methods/array length.js
@@ -0,0 +1,18 @@
+let name="gayathri"
+
+console.log(name.length)
+
+let fruit = "mango"
+
+console.log(fruit.length)
+
+let alphabets= "abcdefghijklmnopqrstuvwxyz"
+
+console.log(alphabets)
+
+console.log(alphabets.length)
+
+
+let animal = ["monkey"]
+
+console.log(animal.length)
