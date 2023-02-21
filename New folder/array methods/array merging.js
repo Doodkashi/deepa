843e6ba9edaf321741846704dd351c97fc22c779
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 21:34:11 +0530

    merging

---
+++ New folder/array methods/array merging.js
@@ -0,0 +1,16 @@
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+
+console.log(myChildren);
+
+
+const x = [1,2,3,4,5];
+const y= [6,7,8,9];
+
+const xy=x.concat(y);
+
+console.log(xy);
+
+
