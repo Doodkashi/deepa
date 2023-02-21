09b7c5b60b31e1a97ddbabe441b946f69849b24e
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 14:27:37 +0530

    filters

---
+++ New folder/filters/filters.js
@@ -0,0 +1,25 @@
+let arr = [20,34,76,83,9,7,3,4,6]
+
+let a = arr.filter((a)=>{
+    return a<10
+})
+
+console.log(a)
+
+
+let num =[1,2,3,4,5,6,7,8,9]
+
+let b = num.filter((b)=>{
+    return b>5
+})
+
+console.log(b)
+
+
+let arr2 = [20,30,40,50,60]
+
+let c = arr2.filter((c)=>{
+    return c>30
+})
+
+console.log(c)
