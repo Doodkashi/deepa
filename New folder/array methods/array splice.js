e9901519d762a9f3713c1b17f3df2bfa6a78d17a
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 18:11:00 +0530

    splice

---
+++ New folder/array methods/array splice.js
@@ -0,0 +1,16 @@
+const names = ["gayathri","amar","deepa","kanna"]
+names.splice(2,3)
+
+console.log(names)
+
+
+const fruits = ["mango","apple","banana","pineapple"]
+fruits.splice(1,3)
+
+console.log(fruits)
+
+
+const students = ["ramu","sita","geeta","rita"]
+students.splice(1,2)
+
+console.log(students)
