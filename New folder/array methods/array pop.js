92e3046f2d86a16a3399270d7eaac500116655d7
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 20:49:00 +0530

    pop array

---
+++ New folder/array methods/array pop.js
@@ -0,0 +1,15 @@
+let names = ["mohan","seeta","geeta","reeta"]
+names.pop();
+
+console.log(names);
+
+let number = [1,2,3,4]
+number.pop();
+
+console.log(number);
+
+
+let fruits = ["mango","apple","banana","papaya"]
+fruits.pop();
+
+console.log(fruits)
