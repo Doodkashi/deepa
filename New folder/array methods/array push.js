9d179e2c8fd6aa8ccc39a38de4fcd9901e86aaa6
doodkashi <suryavamshigayathri@gmail.com>
2023-02-21 20:56:36 +0530

    push array

---
+++ New folder/array methods/array push.js
@@ -0,0 +1,10 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");
+
+console.log(fruits);
+
+
+const names = ["radhika","chandika","meena","reena"];
+names.push("teena");
+
+console.groupCollapsed(names)
