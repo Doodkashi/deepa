9243074ca3d52ba5e197614eff659c61bcaf30c9
doodkashi <suryavamshigayathri@gmail.com>
2023-02-22 10:28:42 +0530

    sorting array

---
+++ New folder/array methods/array sorting.js
@@ -0,0 +1,10 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+
+console.log(fruits);
+
+
+const names = ["gayathri","mounika","deepa","roopa"];
+names.sort();
+
+console.log(names);
